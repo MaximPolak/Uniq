bd2d8e15f81b8fbbc0c2d2932c85bca625946f9a
Maxim Polák <polakmaxi@gmail.com>
2021-05-06 17:13:48 +0200

    Počítání stejných řádků

--- Uniq.py
+++ Uniq.py
@@ -1,6 +1,12 @@
 import sys
 predchozi_radek = None
+pocitadlo = 0
 for radek in sys.stdin:
-    if predchozi_radek != radek:
-        print(radek, end="")
+    if predchozi_radek in (None, radek):
+        pocitadlo += 1 
+    else:
+        print(f"{str(pocitadlo).rjust(3)} {predchozi_radek}", end="")
+        pocitadlo = 1
     predchozi_radek = radek
+
+print(f"{str(pocitadlo).rjust(3)} {predchozi_radek}", end="")
